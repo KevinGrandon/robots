a7e4fe82109d89860712231e497b01caaffb1a43
Kevin Grandon <kevingrandon@yahoo.com>
2014-01-10 21:11:38 -0800

    Add script to manual control robot

--- 90-owi-robot-arm-hacked/cli_all_motors.js
+++ 90-owi-robot-arm-hacked/cli_all_motors.js
@@ -2,7 +2,6 @@ var pinio = new (require('pinio')).Pinio()
 
 pinio.on('ready', function(board) {
 	var motors = require('./motorconfig').getMotors(board)
-	var testMotor = motors[1]
 
 	var keypress = require('keypress');
 
@@ -11,27 +10,33 @@ pinio.on('ready', function(board) {
 
 	process.stdin.setRawMode(true);
 
+	var commandMap = {
+		q: [0, 'ccw'],
+		a: [0, 'cw'],
+		w: [1, 'ccw'],
+		s: [1, 'cw'],
+		e: [2, 'ccw'],
+		d: [2, 'cw'],
+		r: [3, 'ccw'],
+		f: [3, 'cw'],
+		t: [4, 'ccw'],
+		g: [4, 'cw']
+	}
+
 	// listen for the "keypress" event
 	process.stdin.on('keypress', function (ch, key) {
         console.log('Key: ' + key.name);
-
 		if (key && key.ctrl && key.name == 'c') {
 			console.log("Exiting")
 			process.exit()
 		}
 
-        if (key.name === 'left') {
-            console.log('left!')
-           	testMotor.ccw()
-			setTimeout(function() {
-				testMotor.stop()
-			}, 200)
-        } else if (key.name === 'right') {
-           	testMotor.cw()
-			setTimeout(function() {
-				testMotor.stop()
-			}, 200)
-        }
+		var keyConfig = commandMap[key.name]
+		var motor = motors[keyConfig[0]]
+		motor[keyConfig[1]]()
+		setTimeout(function() {
+			motor.stop()
+		}, 200)
 	});
 
 })
--- 90-owi-robot-arm-hacked/motorconfig.js
+++ 90-owi-robot-arm-hacked/motorconfig.js
@@ -4,48 +4,48 @@ exports.getMotors =  function(board) {
 	return [
 		// Base Rotate
 		new Motor({
-			pot: 'A5',
+			pot: 'A1',
 			min: 450,
 			max: 600,
 			home: 500,
 			dir1: 5,
 			dir2: 4
 		}, board),
-		// Claw Open/Close
+		// Shoulder Tilt
 		new Motor({
-			pot: 'A1',
-			min: 250,
-			max: 430,
-			home: 350,
-			dir1: 6,
-			dir2: 7
-		}, board),
-		// Wrist Tilt
-		new Motor({
-			pot: 'A3',
-			min: 570,
-			max: 600,
-			home: 595,
-			dir1: 9,
-			dir2: 8
+			pot: 'A2',
+			min: 450,
+			max: 640,
+			home: 540,
+			dir1: 3,
+			dir2: 2
 		}, board),
 		// Elbow Tilt
 		new Motor({
-			pot: 'A2',
+			pot: 'A3',
 			min: 475,
 			max: 600,
 			home: 540,
 			dir1: 10,
 			dir2: 12
 		}, board),
-		// Shoulder Tilt
+		// Wrist Tilt
 		new Motor({
 			pot: 'A4',
-			min: 450,
-			max: 640,
-			home: 540,
-			dir1: 3,
-			dir2: 2
+			min: 570,
+			max: 600,
+			home: 595,
+			dir1: 9,
+			dir2: 8
+		}, board),
+		// Claw Open/Close
+		new Motor({
+			pot: 'A5',
+			min: 250,
+			max: 430,
+			home: 350,
+			dir1: 6,
+			dir2: 7
 		}, board)
 	]
 }
