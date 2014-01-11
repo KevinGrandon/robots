f687ef5b783fdc6a14a1ee0e5cb46f8747131b26
Kevin Grandon <kevingrandon@yahoo.com>
2014-01-10 22:31:40 -0800

    Simple leap motion finger control of claw.

--- 90-owi-robot-arm-hacked/robot_arm.js
+++ 90-owi-robot-arm-hacked/robot_arm.js
@@ -2,22 +2,49 @@ var pinio = new (require('pinio')).Pinio()
 var webSocket = require('ws')
 var leapSocket = new webSocket('ws://127.0.0.1:6437')
 
+/**
+ * Normalizes leap motion data for a specific motor.
+ * @param {Object} motor object.
+ * @param {Integer} reading of leap motion value.
+ * @param {Integer} minBound. We scale this against the motor minimum bound.
+ * @param {Integer} maxBound. We scale this against the motor maximum bound.
+ */
+function normalize(motor, reading, minBound, maxBound) {
+	var leapDiff = maxBound - minBound
+	var motorDiff = motor.max - motor.min
+	var per = reading/leapDiff
+	var result = per * motorDiff + motor.min
+
+	if (result > motor.max) result = motor.max
+	if (result < motor.min) result = motor.min
+
+	return result
+}
+
 pinio.on('ready', function(board) {
 	var motors = require('./motorconfig').getMotors(board)
 
 	// Wait for potentiometers to read before moving
 	setTimeout(init, 2000)
 
+	var fingerDistance = 0
+
 	function init() {
 		leapSocket.on('message', function(data, flags) {
 			data = JSON.parse(data)
+
+			// Populate finger data
 			if (data.pointables && data.pointables.length === 2) {
 				var distance = data.pointables[1].tipPosition[0] - data.pointables[0].tipPosition[0]
-				var clawGrasp = motors[1]
-				console.log('Pointer distance: ', distance)
-				clawGrasp.moveTo(distance + 300)
+				fingerDistance = distance
 			}
 		})
+
+		setInterval(function() {
+			// Leap bounds: -30 and -100
+			var fingers = motors[4]
+			fingers.moveTo(normalize(fingers, fingerDistance, -30, -100))
+		}, 1000)
 	}
 
 })
