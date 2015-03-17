a63238226dd4142abcf4886eeabc1f9b871e94fd
Kevin Grandon <kevingrandon@yahoo.com>
2015-03-16 21:37:47 -0700

    Capture video stream at a faster FPS.

--- 81-hiro-vr-bot/code/hiro.js
+++ 81-hiro-vr-bot/code/hiro.js
@@ -42,7 +42,7 @@ function stopStreaming() {
   if (Object.keys(sockets).length == 0) {
     app.set('watchingFile', false)
     if (proc) proc.kill()
-    fs.unwatchFile('./stream/image_stream.jpg')
+    fs.unwatchFile(__dirname + '/www/image_stream.jpg')
   }
 }
 
@@ -52,15 +52,16 @@ function startStreaming(io) {
     return
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"]
+  // Try to get to 25 fps
+  var cameraTick = 40;
+  var args = ["-w", "640", "-h", "480", "-o", __dirname + "/www/image_stream.jpg", "-t", "999999999", "-tl", String(cameraTick)]
   proc = spawn('raspistill', args)
-
   console.log('Watching for changes...')
 
   app.set('watchingFile', true)
 
-  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-    io.sockets.emit('stream:data', 'image_stream.jpg?_t=' + (Math.random() * 100000))
+  fs.watchFile(__dirname + '/www/image_stream.jpg', {persistent: true, interval: cameraTick}, function(current, previous) {
+	io.sockets.emit('stream:data', 'image_stream.jpg?_t=' + (Math.random() * 100000))
   })
 }
 
@@ -85,7 +86,7 @@ io.on('connection', function (socket) {
 		if (Object.keys(sockets).length == 0) {
 			app.set('watchingFile', false)
 			if (proc) proc.kill()
-			fs.unwatchFile('./stream/image_stream.jpg')
+			fs.unwatchFile(__dirname + '/www/image_stream.jpg')
 		}
 	});
 
