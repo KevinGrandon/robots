11e96a213a82e43e8b88eb235f0a6df932951b1c
Kevin Grandon <kevingrandon@yahoo.com>
2015-04-03 16:18:27 -0700

    Add leg id definitions.

--- spider.ino
+++ spider.ino
@@ -5,7 +5,37 @@ BioloidController bioloid = BioloidController(1000000);
 
 // Octopod pin constants.
 // Looking down at the octopod's back, with the head at the top.
-int 
+#define l1CoxaId     0   // Front Left leg Hip Horizontal
+#define l1FemurId    8   // Front Left leg Hip Vertical
+#define l1TibiaId    16  // Front Left leg Knee
+
+#define l2CoxaId     1   // Middle Left Front leg Hip Horizontal
+#define l2FemurId    9   // Middle Left Front leg Hip Vertical
+#define l2TibiaId    17  // Middle Left Front leg Knee
+
+#define l3CoxaId     2   // Middle Left Rear leg Hip Horizontal
+#define l3FemurId    10  // Middle Left Rear leg Hip Vertical
+#define l3TibiaId    18  // Middle Left Rear leg Knee
+
+#define l4CoxaId     3   // Rear Left leg Hip Horizontal
+#define l4FemurId    11  // Rear Left leg Hip Vertical
+#define l4TibiaId    19  // Rear Left leg Knee
+
+#define r1CoxaId     7   // Front Right leg Hip Horizontal
+#define r1FemurId    15  // Front Right leg Hip Vertical
+#define r1TibiaId    23  // Front Right leg Knee
+
+#define r2CoxaId     6   // Middle Right Front leg Hip Horizontal
+#define r2FemurId    14  // Middle Right Front leg Hip Vertical
+#define r2TibiaId    22  // Middle Right Front leg Knee
+
+#define r3CoxaId     5   // Middle Right Rear leg Hip Horizontal
+#define r3FemurId    13  // Middle Right Rear leg Hip Vertical
+#define r3TibiaId    21  // Middle Right Rear leg Knee
+
+#define r4CoxaId     4   // Rear Right leg Hip Horizontal
+#define r4FemurId    12  // Rear Right leg Hip Vertical
+#define r4TibiaId    20  // Rear Right leg Knee
 
 // AX Servo Constants
 #define AX_SYNC_WRITE 131
