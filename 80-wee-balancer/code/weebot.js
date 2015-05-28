04a2fc167cc5ea88505ae96ebf0365e6d77b3b0a
Kevin Grandon <kevingrandon@yahoo.com>
2015-05-28 14:11:26 -0400

    Initial PID implementation.

--- 80-wee-balancer/code/weebot.js
+++ 80-wee-balancer/code/weebot.js
@@ -3,6 +3,21 @@ var board = new five.Board();
 
 board.on('ready', function() {
 
+  var rollAngle = 0;
+
+  // PID constants
+  var kp = 100;         // Proportional value
+  var ki = 2;           // Integral value
+  var kd = 0;           // Derivative value
+
+  // PID variables
+  var outMax = 255;
+  var outMin = -255;
+  var lastInput = 0;
+  var iTerm = 0;
+
+  var setPoint = 0;
+
   var motor1 = new five.Motor({
     pins: {
       pwm: 9,
@@ -16,6 +31,41 @@ board.on('ready', function() {
       dir: 8
     }
   });
+
+  // Angular velocity of roll from gyroscope
+  function getDGyroRoll(gyro){
+    return gyro.roll.rate;
+  }
+
+  // Roll from accelerometer
+  function getAccelRoll(accel) {
+    return accel.roll;
+  }
+
+  function computeSpeed(input) {
+    var error = setPoint - input;
+    iTerm += (ki * error);
+    if (iTerm > outMax) {
+      iTerm = outMax;
+    } else if(iTerm < outMin) {
+      iTerm = outMin;
+    }
+    var dInput = (input - lastInput);
+
+    // Compute PID Output
+    var output = kp * error + iTerm + kd * dInput;
+
+    if (output > outMax) {
+      output = outMax;
+    } else if (output < outMin) {
+      output = outMin;
+    }
+
+    // Remember some variables for next time
+    lastInput = input;
+    console.log('PID output:', output);
+    return output;
+  }
 
   function motorControl(out) {
     var velocity = Math.abs(out);
@@ -40,6 +90,7 @@ board.on('ready', function() {
   });
 
   imu.on('change', function() {  
+    /*
     console.log('accelerometer');
     console.log('  x            : ', this.accelerometer.x);
     console.log('  y            : ', this.accelerometer.y);
@@ -61,6 +112,10 @@ board.on('ready', function() {
     console.log('  rate         : ', this.gyro.rate);
     console.log('  isCalibrated : ', this.gyro.isCalibrated);
     console.log('--------------------------------------');    
+    */
+
+    rollAngle = 0.98 * (rollAngle + getDGyroRoll(this.gyro)) + 0.02 * (getAccelRoll(this.accelerometer));
+    motorControl(computeSpeed(rollAngle));
   });
 
 });
